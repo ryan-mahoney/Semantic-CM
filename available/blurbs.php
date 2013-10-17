f44f15ea9c4a6f5e3c9cc8c2f49f1ebebb763cf0
webuser <ryan@virtuecenter.com>
2013-10-17 08:56:40 -0400

    prepare for package manager

---
+++ available/blurbs.php
@@ -0,0 +1,5 @@
+<?php
+class blurbs {
+	public $api = '/some/path';
+	public 
+}
---
+++ src/Manager/Manager.php
@@ -0,0 +1,17 @@
+<?php
+namespace Manager;
+
+class Manager {
+
+	public function factory ($manager) {
+
+	}
+
+	public function table ($managerObject) {
+		$this->separation->app()->layout()->write();
+	}
+
+	public function form ($managerObject) {
+
+	}
+}
