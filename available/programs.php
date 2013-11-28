8877527039df96611cfa255f064a38f4b2b6fcfc
Zeeshan Sattar <zeeshan@virtuecenter.com>
2013-11-28 16:22:44 -0500

    program and social-links

---
+++ available/programs.php
@@ -0,0 +1,290 @@
+<?php
+/*  Copyright 2011 Ryan Mahoney
+ *
+ *  This file is part of the Nothing Framework.
+ *
+ *  Nothing Framework is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Nothing Framework is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Nothing Framework.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
+ * @version .1
+ * @link https://raw.github.com/virtuecenter/manager/master/available/programs.php
+ * @mode upgrade
+ */
+
+
+namespace Manager;
+class programs {
+	private $field = false;
+    public $collection = 'programs';
+    public $form = 'programs';
+    public $title = 'Programs';
+    public $singular = 'Program';
+    public $description = '4 programs';
+    public $definition = '...';
+    public $acl = ['content', 'admin', 'superadmin'];
+    public $tabs = ['Main', 'Images', 'SEO'];
+    public $icon = 'Grid';
+    public $category = 'Content';
+    public $notice = 'Programs Saved';
+    public $storage = [
+        'collection' => 'programs',
+        'key' => '_id'
+    ];
+
+    function titleField () {
+		return [
+			'name'		=> 'title',
+			'label'		=> 'Title',
+			'required'	=> true,
+			'display'	=> 'InputText'
+		];
+	}
+
+	function bodyField () {
+		return [
+			'display' => 'Ckeditor',
+			'name' => 'body'
+		];
+	}
+
+	function descriptionField () {
+		return [
+			'name' => 'description',
+			'label' => 'Summary',
+			'display' => 'Textarea'
+		];
+	}
+
+	
+	function imageField () {
+		return [
+			'name' => 'image',
+			'label' => 'List View',
+			'display' => 'InputFile'
+		];
+	}
+
+	function imageFeaturedField () {
+		return [
+			'name' => 'image_feature',
+			'label' => 'Featured View',
+			'display' => 'InputFile'
+		];
+	}
+	
+
+    function code_nameField () {
+		return [
+			'name' => 'code_name',
+			'display'	=> 'InputText'
+		];
+	}
+
+	function metakeywordsField () {
+		return [
+			'name' => 'metadata_keywords',
+			'display'	=> 'InputText'
+		];
+	}
+
+	function metadescriptionField () {
+		return [
+			'name' => 'metadata_description',
+			'display'	=> 'InputText'
+		];
+	}
+
+	function statusField () {
+		return [
+			'name'		=> 'status',
+			'required'	=> true,
+			'options'	=> array(
+				'published'	=> 'Published',
+				'draft'		=> 'Draft'
+			),
+			'display'	=> 'Select',
+			'nullable'	=> false,
+			'default'	=> 'published'
+		];
+	}
+
+	function featuredField () {
+        return [
+            'name' => 'featured',
+            'label' => 'Feature',
+            'required' => false,
+            'options' => array(
+                't' => 'Yes',
+                'f' => 'No'
+            ),
+            'display' => 'InputSlider',
+            'default' => 'f'
+        ];
+    }
+
+
+    function pinnedField () {
+        return [
+            'name' => 'pinned',
+            'label' => 'Pin',
+            'required' => false,
+            'options' => array(
+                't' => 'Yes',
+                'f' => 'No'
+            ),
+            'display' => 'InputSlider',
+            'default' => 'f'
+        ];
+    }
+
+    function tagsField () {
+		return [
+			'name' => 'tags',
+			'label' => 'Tags',
+			'required' => false,
+			'transformIn' => function ($data) {
+				if (is_array($data)) {
+					return $data;
+				}
+				return $this->field->csvToArray($data);
+			},
+			'display' => 'InputToTags',
+			'multiple' => true,
+			'options' => function () {
+				return $this->db->distinct('blogs', 'tags');
+			}
+		];
+	}
+/*
+	public function homepage_image_wideField() {
+		return array(
+			'name'		=> 'homepage_image_wide',
+			'label' 	=> 'Home Page Image',
+			'display'   => VCPF\Field::inputFile()	
+		);
+	}	
+		
+	function featuredField () {
+		return array(
+			'name' => 'featured',
+			'label' => false,
+			'required' => false,
+			'options' => array(
+				't' => 'Yes',
+				'f' => 'No'
+			),
+			'display' => VCPF\Field::inputRadioButton(),
+			'default' => 'f'
+			);
+	}
+	*/
+	public function tablePartial () {
+        $partial = <<<'HBS'
+            <div class="top-container">
+                {{#CollectionHeader}}{{/CollectionHeader}}
+            </div>
+
+            <div class="bottom-container">
+                {{#CollectionPagination}}{{/CollectionPagination}}
+                {{#CollectionButtons}}{{/CollectionButtons}}
+                
+                <table class="ui large table segment manager">
+                    <thead>
+                        <tr>
+                            <th>Title</th>
+                            <th>Category</th>
+                            <th>Status</th>
+                            <th class="trash">Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {{#each programs}}
+                            <tr data-id="{{dbURI}}">
+                                <td>{{title}}</td>
+                                <td>{{category}}</td>
+                                <td>{{status}}</td>
+                                <td>
+                                    <div class="manager trash ui icon button">
+                                         <i class="trash icon"></i>
+                                     </div>
+                                 </td>
+                            </tr>
+                        {{/each}}
+                    </tbody>
+                </table>
+
+                {{#CollectionPagination}}{{/CollectionPagination}}
+            </div>
+HBS;
+        return $partial;
+    }
+
+    public function formPartial () {
+        $partial = <<<'HBS'
+	        {{#Form}}{{/Form}}
+	            <div class="top-container">
+	                {{#DocumentHeader}}{{/DocumentHeader}}
+	                {{#DocumentTabs}}{{/DocumentTabs}}
+	            </div>
+
+	            <div class="bottom-container">
+	            	<div class="ui tab active" data-tab="Main">
+		                {{#DocumentFormLeft}}
+		                    {{#FieldLeft title Title required}}{{/FieldLeft}}
+		                    {{#FieldLeft body Body}}{{/FieldLeft}}
+		                    {{#FieldLeft description Summary}}{{/FieldLeft}}
+		                    {{{id}}}
+		                {{/DocumentFormLeft}}                 
+		                
+		                {{#DocumentFormRight}}
+		                	{{#DocumentButton}}{{/DocumentButton}}
+		                	{{#FieldFull status}}{{/FieldFull}}
+		                	<div class="ui clearing divider"></div>
+		                	{{#FieldLeft featured}}{{/FieldLeft}}
+		                	<br />
+		                	{{#FieldLeft pinned}}{{/FieldLeft}}
+		                	<div class="ui clearing divider"></div>
+		                	{{#FieldFull tags Tags}}{{/FieldFull}}
+		                {{/DocumentFormRight}}
+		            </div>
+
+		             <div class="ui tab" data-tab="Images">
+		                {{#DocumentFormLeft}}
+		                    {{#FieldLeft image List View Image}}{{/FieldLeft}}
+		                    {{#FieldLeft image_feature Featured View Image}}{{/FieldLeft}}
+		                    {{#FieldLeft }}{{/FieldLeft}}
+		                    {{#FieldLeft }}{{/FieldLeft}}
+		                {{/DocumentFormLeft}}                 
+		                
+		                {{#DocumentFormRight}}
+			                {{#DocumentButton}}{{/DocumentButton}}
+		                {{/DocumentFormRight}}
+		            </div>
+		            <div class="ui tab" data-tab="SEO">
+		            	 {{#DocumentFormLeft}}
+		                    {{#FieldLeft code_name Slug}}{{/FieldLeft}}
+		                    {{#FieldLeft metadata_description Description}}{{/FieldLeft}}
+		              		{{#FieldLeft metadata_keywords Keywords}}{{/FieldLeft}}
+		                {{/DocumentFormLeft}}
+		                
+		                {{#DocumentFormRight}}
+			                {{#DocumentButton}}{{/DocumentButton}}
+		                {{/DocumentFormRight}}
+		            </div>	        
+	            </div>
+	        </form>
+HBS;
+        return $partial;
+    }
+}
