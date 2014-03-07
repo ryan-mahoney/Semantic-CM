68716fcae53dd93c5e084057e456ce946df8df51
Zeeshan Sattar <zeeshan@virtuecenter.com>
2014-03-07 15:37:03 -0500

    updated

---
+++ available/system_messages.php
@@ -0,0 +1,213 @@
+<?php
+/*
+ * @version .3
+ * @link https://raw.github.com/virtuecenter/manager/master/available/system_messages.php
+ * @mode upgrade
+ * 
+ */
+namespace Manager;
+
+class system_messages {
+	private $field = false;
+    public $collection = 'system_messages';
+    public $title = 'System Messages';
+    public $titleField = 'name';
+    public $singular = 'Message';
+    public $description = '{{count}} messages';
+    public $definition = 'Coming Soon';
+    public $acl = ['content', 'admin', 'superadmin'];
+    public $tabs = ['Content'];
+    public $icon = 'text file';
+    public $category = 'Content';
+    public $after = 'function';
+    public $function = 'ManagerSaved';
+    public $storage = [
+        'collection' => 'system_messages',
+        'key' => '_id'
+    ];
+
+    public function __construct ($field=false) {
+        $this->field = $field;
+    }
+
+	function nameField() {
+    return [
+      'name'    => 'name',
+      'placeholder' => 'Name',
+      'display' => 'InputText',
+      'required'  => true
+    ];
+  }
+  
+  function subjectField() {
+    return [
+      'name'    => 'subject',
+      'placeholder' => 'Subject',
+      'label'   => 'Subject',
+      'display' => 'InputText',
+      'required'  => true
+    ];
+  }
+
+	function bodyField () {
+    return [
+      'name' => 'body',
+      'label' => 'Body',
+      'required' => false,
+      'display' => 'Ckeditor'
+    ];
+  }
+
+  function fromField() {
+    return [
+      'name'    => 'from',
+      'placeholder' => 'From',
+      'display' => 'InputToTags',
+      'controlled' => false,
+      'multiple' => false,
+      'required'  => true,
+      'options' => function () {
+        return $this->db->distinct('system_messages', 'from');
+      }
+    ];
+  }  
+
+  function replyToField() {
+    return [
+      'name'    => 'replyTo',
+      'placeholder' => 'Reply To',
+      'display' => 'InputToTags',
+      'controlled' => false,
+      'multiple' => false,
+      'required'  => false,
+      'options' => function () {
+        return $this->db->distinct('system_messages', 'replyTo');
+      }
+    ];
+  }
+
+  function ccField() {
+    return [
+      'name'    => 'cc',
+      'placeholder' => 'CC',
+      'display' => 'InputToTags',
+      'multiple' => true,
+      'controlled' => false,
+      'required'  => false,
+      'options' => function () {
+        return $this->db->distinct('system_messages', 'cc');
+      }
+    ];
+  }
+
+  function bccField() {
+    return [
+      'name'    => 'bcc',
+      'placeholder' => 'Bcc',
+      'display' => 'InputToTags',
+      'multiple' => true,
+      'controlled' => false,
+      'required'  => false,
+      'options' => function () {
+        return $this->db->distinct('system_messages', 'Bcc');
+      }
+    ];
+  }
+
+  function tagsField () {
+    return [
+      'name' => 'tags',
+      'label' => 'Tags',
+      'required' => false,
+      'transformIn' => function ($data) {
+        if (is_array($data)) {
+          return $data;
+        }
+        return $this->field->csvToArray($data);
+      },
+      'display' => 'InputToTags',
+      'multiple' => true,
+      'controlled' => false,
+      'options' => function () {
+        return $this->db->distinct('system_messages', 'tags');
+      }
+    ];
+  }
+
+    public function tablePartial () {
+        $partial = <<<'HBS'
+            <div class="top-container">
+                {{#CollectionHeader}}{{/CollectionHeader}}
+            </div>
+
+           <div class="bottom-container">
+              {{#if system_messages}}
+                    {{#CollectionPagination}}{{/CollectionPagination}}
+                    {{#CollectionButtons}}{{/CollectionButtons}}
+                
+                    <table class="ui large table segment manager">
+                         <thead>
+                               <tr>
+                                  <th>Name</th>
+                                  <th>Subject</th>
+                                  <th>Reply To</th>
+                                  <th class="trash">Delete</th>
+                               </tr>
+                         </thead>
+                         <tbody>
+                            {{#each system_messages}}
+                                 <tr data-id="{{dbURI}}">
+                                     <td>{{name}}</td>
+                                     <td>{{subject}}</td>
+                                      <td>{{replyTo}}</td>
+                                    <td>
+                                       <div class="manager trash ui icon button">
+                                           <i class="trash icon"></i>
+                                       </div>
+                                    </td>
+                                 </tr>
+                            {{/each}}
+                         </tbody>
+                    </table>
+                     {{#CollectionPagination}}{{/CollectionPagination}}
+                {{else}}
+					 {{#CollectionEmpty}}{{/CollectionEmpty}}
+	          {{/if}}
+           </div>
+HBS;
+        return $partial;
+    }
+
+    public function formPartial () {
+        $partial = <<<'HBS'
+	        {{#Form}}{{/Form}}
+	            <div class="top-container">
+	                {{#DocumentHeader}}{{/DocumentHeader}}
+	                {{#DocumentTabs}}{{/DocumentTabs}}
+	            </div>
+
+	            <div class="bottom-container">
+	            	<div class="ui tab active" data-tab="Content">
+		                {{#DocumentFormLeft}}
+                        {{#FieldLeft name Name}}{{/FieldLeft}}
+                        {{#FieldLeft subject Subject}}{{/FieldLeft}}
+		                    {{#FieldLeft body Body}}{{/FieldLeft}}
+		                    {{{id}}}
+		                {{/DocumentFormLeft}}                 
+		                
+		                {{#DocumentFormRight}}
+		                	{{#DocumentButton}}{{/DocumentButton}}
+		                	{{#FieldFull from From}}{{/FieldFull}}
+                      {{#FieldFull replyTo "Reply To"}}{{/FieldFull}}
+                      {{#FieldFull cc CC}}{{/FieldFull}}
+                      {{#FieldFull bcc Bcc}}{{/FieldFull}}
+		                	<div class="ui clearing divider"></div>
+                      {{#FieldFull tags Tags}}{{/FieldFull}}
+		                {{/DocumentFormRight}}
+		            </div>	       
+	            </div>
+	        </form>
+HBS;
+        return $partial;
+    }
+}	
---
+++ available/users_address.php
@@ -0,0 +1,219 @@
+<?php
+/*
+ * @version .3
+ * @link https://raw.github.com/virtuecenter/manager/master/available/users_address.php
+ * @mode upgrade
+ *
+ */
+namespace Manager;
+
+class users_address {
+	private $field = false;
+	public $collection = 'users';
+	public $title = 'address';
+	public $titleField = 'title';
+	public $singular = 'Address';
+	public $description = '{{count}} address';
+	public $definition = 'Coming Soon';
+	public $acl = ['content', 'admin', 'superadmin'];
+	public $icon = 'browser';
+	public $category = 'Content';
+	public $after = 'function';
+	public $function = 'embeddedUpsert';
+	public $notice = 'Address Saved';
+	public $embedded = true;
+	public $storage = [
+		'collection' => 'users',
+		'key' => '_id'
+	];
+
+	public function __construct ($field=false) {
+		$this->field = $field;
+	}
+
+	function typeField () {
+		return [
+			'name'		=> 'type',
+			'required'	=> true,
+			'options'	=> array(
+				'home'	=> 'Home',
+				'work'		=> 'Work'
+			),
+			'display'	=> 'Select',
+			'nullable'	=> false,
+			'default'	=> 'blank'
+		];
+	}
+
+	function addressField () {
+		return [
+			'name' => 'address',
+			'label' => 'Address',
+			'required' => false,
+			'display' => 'Textarea'
+		];
+	}
+
+	function address2Field () {
+		return [
+			'name' => 'address2',
+			'label' => 'Address2',
+			'required' => false,
+			'display' => 'Textarea'
+		];
+	}
+
+	function cityField () {
+		return [
+			'name' 		=> 'city',
+			'label' 	=> 'City',
+			'required' 	=> true,
+			'display' 	=> 'InputText'	
+		];
+	}
+
+	function stateField () {
+		return [
+			'name' 		=> 'state',
+			'label' 	=> 'State',
+			'required' 	=> false,
+			'options' 	=> array(
+				'AL'=>"Alabama",
+				'AK'=>"Alaska",
+				'AS'=>"American Samoa",
+				'AZ'=>"Arizona",
+				'AR'=>"Arkansas",
+				'CA'=>"California",
+				'CO'=>"Colorado",
+				'CT'=>"Connecticut",
+				'DE'=>"Delaware",
+				'DC'=>"District Of Columbia",
+				'FM'=>"Federated States of Micronesia",
+				'FL'=>"Florida",'GA'=>"Georgia",
+				'GU'=>"Guam",
+				'HI'=>"Hawaii",
+				'ID'=>"Idaho",
+				'IL'=>"Illinois",
+				'IN'=>"Indiana",
+		 		'IA'=>"Iowa",
+		 		'KS'=>"Kansas",
+				'KY'=>"Kentucky",
+				'LA'=>"Louisiana",
+				'ME'=>"Maine",
+				'MH'=>"Marshall Islands",
+				'MD'=>"Maryland",
+			 	'MA'=>"Massachusetts",
+				'MI'=>"Michigan",
+				'MN'=>"Minnesota",
+				'MS'=>"Mississippi",
+				'MO'=>"Missouri",
+				'MT'=>"Montana",
+				'NE'=>"Nebraska",
+				'NV'=>"Nevada",
+				'NH'=>"New Hampshire",
+				'NJ'=>"New Jersey",
+				'NM'=>"New Mexico",
+				'NY'=>"New York",
+				'NC'=>"North Carolina",
+				'ND'=>"North Dakota",
+				'MP'=>"Northern Mariana Islands",
+				'OH'=>"Ohio",
+				'OK'=>"Oklahoma",
+				'OR'=>"Oregon",
+				'PW'=>"Palau",
+				'PA'=>"Pennsylvania",
+				'PR'=>"Puerto Rico",
+				'RI'=>"Rhode Island",
+				'SC'=>"South Carolina",
+				'SD'=>"South Dakota",
+				'TN'=>"Tennessee",
+				'TX'=>"Texas",
+				'UT'=>"Utah",
+				'VT'=>"Vermont",
+				'VI'=>"Virgin Islands",
+				'VA'=>"Virginia",
+				'WA'=>"Washington",
+				'WV'=>"West Virginia",
+				'WI'=>"Wisconsin",
+				'WY'=>"Wyoming",
+				'AA'=>"Armed Forces Americas", 
+				'AE'=>"Armed Forces", 
+				'AP'=>"Armed Forces Pacific" 
+					),
+				'display' 	=> 'Select',
+				'nullable' 	=> true
+			];
+		}
+	
+	function zipcodeField () {
+		return [
+			'name' 		=> 'zip',
+			'label' 	=> 'Zip',
+			'required' 	=> true,
+			'display' 	=> 'InputText'	
+		];
+	}
+
+	function primaryField () {
+        return [
+            'name' => 'primary',
+            'label' => 'Primary',
+            'required' => false,
+            'options' => array(
+                't' => 'Yes',
+                'f' => 'No'
+            ),
+            'display' => 'InputSlider',
+            'default' => 'f'
+        ];
+    }
+
+
+	public function tablePartial () {
+		$partial = <<<'HBS'
+			{{#EmbeddedCollectionHeader label="Address"}}
+			{{#if address_sub}}
+				<table class="ui table manager segment">
+					<thead>
+						<tr>
+							<th>Type</th>
+							<th>State</th>
+							<th>Primary</th>
+							<th class="trash">Delete</th>
+						</tr>
+					</thead>
+					<tbody>
+						{{#each address_sub}}
+							<tr data-id="{{dbURI}}">
+								<td>{{type}}</td>
+								<td>{{state}}</td>
+								<td>{{primary}}</td>
+								<td><div class="manager trash ui icon button"><i class="trash icon small"></i></div></td>
+							</tr>
+						{{/each}}
+					</tbody>
+				</table>
+		    {{else}}
+			    {{#EmbeddedCollectionEmpty singular="Address"}}
+	        {{/if}}
+HBS;
+		return $partial;
+	}
+
+	public function formPartial () {
+		$partial = <<<'HBS'
+			{{#EmbeddedHeader}}
+			{{#FieldFull type Type}}{{/FieldFull}}
+			{{#FieldFull address Address}}{{/FieldFull}}
+			{{#FieldFull address2 Address2}}{{/FieldFull}}
+			{{#FieldFull city City}}{{/FieldFull}}
+			{{#FieldFull state State}}{{/FieldFull}}
+			{#FieldFull zip Zip}}{{/FieldFull}}
+	        {{#FieldFull primary Primary}}{{/FieldFull}}
+		    {{{id}}}
+			{{#EmbeddedFooter}}
+HBS;
+		return $partial;
+	}
+}
+
