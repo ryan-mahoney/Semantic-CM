33a34adea45062f59e488802dde3ec67a938b232
rmahoney-bsd <rmahoney@bluestatedigital.com>
2014-05-06 23:07:11 -0400

    standardize

---
+++ tests/Semantic-CMTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace Opine;
+
+class Semantic-CMTest extends \PHPUnit_Framework_TestCase {
+    private $db;
+
+    public function setup () {
+        date_default_timezone_set('UTC');
+        $root = getcwd();
+        $container = new Container($root, $root . '/container.yml');
+        $this->db = $container->db;
+    }
+
+    public function testSample () {
+        $this->assertTrue(true);
+    }
+}
